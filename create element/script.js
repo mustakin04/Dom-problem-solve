971b5b565b2831595af26bb1752c41c5a48a3768
mustakin04 <mustakinhasan37@gmail.com>
2024-09-30 01:26:56 +0600

    create tag

---
+++ create element/script.js
@@ -0,0 +1,5 @@
+let place=document.querySelector(".place");
+let head=document.createElement("h1");
+let text=document.createTextNode("I am a Hero");
+head.appendChild(text);
+place.appendChild(head);
